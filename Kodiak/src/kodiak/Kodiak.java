3b34eb244911df4ff3ea9b5a8cd3ff1f533571aa
Chris <cralor@gmail.com>
2013-08-01 00:08:56 -0400

    Removed setters for date and time Added time updates and correct date setup

--- Kodiak/src/kodiak/Kodiak.java
+++ Kodiak/src/kodiak/Kodiak.java
@@ -4,6 +4,9 @@
  */
 package kodiak;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -12,17 +15,18 @@ import javax.swing.JPanel;
  * @author Wayne E Starr
  */
 public class Kodiak {
+    
+    static JFrame jfmMain = new JFrame();
+    static JPanel jplMain = new Main();
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        JFrame jfmMain = new JFrame();
-        JPanel jplMain = new Main();
-        
         jfmMain.add(jplMain);
         jfmMain.setTitle("Kodiak");
         jfmMain.pack();
         jfmMain.setVisible(true);
     }
+    
 }
